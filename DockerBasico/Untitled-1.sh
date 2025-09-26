4c5d3785ed339ca6ce1f4a65593d297903ef85b1
RadASIR101 <otracuentamovil102@gmail.com>
2025-09-26 08:59:41 +0200

    2º commit

---
+++ DockerBasico/Untitled-1.sh
@@ -0,0 +1,16 @@
+
+docker run -it --rm --name alpine01 alpine sh
+
+docker ps -a
+
+control p + control q
+
+docker stop 
+
+docker ps -a 
+
+docker rm alpine01
+
+docker run -it --rm --name alpine01 alpine sh
+
+docker run -dit --rm --name alpine01 alpine sh
